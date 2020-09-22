2539c33d490909e2a96c0dd2bad4af2853f9ecf0
Arkady Siuris <arss860@gmail.com>
2020-09-22 22:22:39 +0300

    no more data bindings(it doesn't work)

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.weatherapp
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         updateTheme()
 
         Log.i("A", if (isDarkTheme) "dark theme" else "light")
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.activity_main)
 
         updateSwitch()
 
